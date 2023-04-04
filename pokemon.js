38acd89e7dd5139c868c803220941b7123b524aa
UrojasG <urielelihurojasgarfias@gmail.com>
2023-04-04 15:14:05 -0600

    feat: pokedex web

---
+++ pokemon.js
@@ -0,0 +1,76 @@
+window.onload = main; 
+
+function main(){
+
+    //Llama la función cada 30 ClickBoton()
+    let timer = setInterval(() => {
+        ClickBoton(timer);
+    }, 30*1000);
+
+    //Entra a la función ClickBoton si es presionada
+    let btnLoad = document.getElementById("btnLoad");
+    btnLoad.addEventListener('click', ClickBoton);
+
+}
+
+
+
+//cargar el URL de la api
+async function loadAPI(url){
+    let response = await fetch(url);
+    return await response.json();
+}
+
+
+async function ClickBoton(timer){
+    //Limpia el intervarlo si entra a la función
+    clearInterval(timer);
+
+    //Declaración de variables
+    let randomNumber = Math.floor(Math.random() * (1010 - 1 + 1)) + 1; //Genera número aleatorios
+    let json = await loadAPI(`https://pokeapi.co/api/v2/pokemon-form/${randomNumber}`);
+    let jsonAbilities = await loadAPI(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`);
+    let listaPoke = [];
+    let list_group_item = document.getElementById("Poke-list");
+    
+    //Limpia las etiquetas creadas
+    if(list_group_item.firstChild != null){
+        while (list_group_item.firstChild) {
+            list_group_item.removeChild(list_group_item.firstChild);
+        }
+    }
+    
+
+    
+    //Agregar nombre del pokemon
+    document.getElementById("pokeName").innerHTML = json.name;
+
+    //Agrega las habilidades del pokemon
+    for (let i = 0; i < jsonAbilities.abilities.length; i++) {
+        listaPoke[i] = jsonAbilities.abilities[i].ability.name
+        //console.log(jsonAbilities.abilities[i].ability.name);
+
+        let tag = document.createElement("li");
+        tag.innerHTML = `Habilidad ${i+1}: ${listaPoke[i]}`;
+        list_group_item.appendChild(tag);
+    }
+
+    //Agregar las imágenes del pokemon
+    if(json.sprites.back_default!=null){
+        document.getElementById("pokeImgB").src = json.sprites.back_default;
+    }else document.getElementById("pokeImgB").src = 'https://static.thenounproject.com/png/1496955-200.png';
+    if(json.sprites.back_shiny!=null){
+        document.getElementById("pokeImgD").src = json.sprites.back_shiny;
+    }else document.getElementById("pokeImgD").src = 'https://static.thenounproject.com/png/1496955-200.png';
+    
+    if(json.sprites.front_default!=null){
+        document.getElementById("pokeImgS").src = json.sprites.front_default;
+    }else document.getElementById("pokeImgS").src = 'https://static.thenounproject.com/png/1496955-200.png';
+    
+    if(json.sprites.front_shiny!=null){
+        document.getElementById("pokeImgDS").src = json.sprites.front_shiny;
+    }else document.getElementById("pokeImgDS").src = 'https://static.thenounproject.com/png/1496955-200.png';
+    
+    
+
+}
